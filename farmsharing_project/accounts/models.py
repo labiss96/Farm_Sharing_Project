9cbc0d9e5c6d82a5fc9229138a1e1b598d210eb4
leejunghyun <cxz9080@gmail.com>
2019-11-23 01:21:26 +0900

    기타 파일수정

--- farmsharing_project/accounts/models.py
+++ farmsharing_project/accounts/models.py
@@ -13,3 +13,15 @@ class Profile(AbstractUser):
 
     def __str__(self):
          return self.username
+
+
+class Comment(models.Model):
+    belong_to_user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='profile_comment') #유저 마이페이지에 연결
+    writer = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='writer_comment') #작성자 onetoMany 관계
+    comments = models.TextField(null=False) #댓글내용
+    rating = models.IntegerField(null=True) #별점
+
+class Land(models.Model): #땅
+    region=models.CharField(max_length=200,null=True) #지역
+    land_area=models.CharField(max_length=100,null=True) #면적
+    land_condition=models.CharField(max_length=200,null=True) #땅상태
